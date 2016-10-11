4c2235983cc168b94ee846daffe1b11474dab386
Mustafa Dogan <msdogan@ucdavis.edu>
2016-10-11 10:10:48 -0700

    adding solver runtime comparison

--- job-slurm.sh
+++ job-slurm.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
-#SBATCH -D /home/jdherman/pyvin
-#SBATCH -o /home/jdherman/pyvin/job.%j.%N.out
-#SBATCH -e /home/jdherman/pyvin/job.%j.%N.err
+#SBATCH -D /home/msdogan/pyvin
+#SBATCH -o /home/msdogan/pyvin/job.%j.%N.out
+#SBATCH -e /home/msdogan/pyvin/job.%j.%N.err
 #SBATCH -n 32            # Total number of processors to request (32 cores per node)
 #SBATCH -p med           # Queue name hi/med/lo
 #SBATCH -t 200:00:00        # Run time (hh:mm:ss) - 24 hours
-#SBATCH --mail-user=jdherman@ucdavis.edu              # address for email notification
+#SBATCH --mail-user=msdogan@ucdavis.edu              # address for email notification
 #SBATCH --mail-type=ALL                  # email at Begin and End of job
 
-DIR=statewide-82yr-debug
+# you can define your own variables. Access them later with dollar signs ($DIR, etc.)
+DIR=mustafa-82yr
+GDIR=/group/hermangrp
 
-# pyomo solve --solver=glpk --solver-suffix=dual pyvin.py $DIR/data.dat --json --report-timing --stream-solver
+# IMPORTANT: Python3/Pyomo/CBC solver are all installed in group directory. Add it to the path.
+export PATH=$GDIR/miniconda3/bin:$GDIR/cbc/bin:$PATH
+
+# Export CPLEX and GUROBI solvers
+export CPLEX_HOME=$GDIR/cplex1263/cplex
+export GUROBI_HOME=$GDIR/gurobi652/linux64
+export PATH=$PATH:$GUROBI_HOME/bin:$CPLEX_HOME/bin/x86-64_linux:$GDIR/cbc/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GUROBI_HOME/lib:$CPLEX_HOME/lib/x86-64_linux/static_pic
+export GRB_LICENSE_FILE=$GDIR/gurobi652/gurobi.lic
+
+# if you want GLPK (serial)
+#pyomo solve --solver=glpk --solver-suffix=dual pyvin.py $DIR/data.dat --json --report-timing --stream-solver
+
+# if you want CBC (parallel, can only use up to 32 cores)
+# if you want to use CPLEX or GUROBI solvers, change "--solver=cbc" to "--solver=cplex" or "--solver=gurobi"
 pyomo solve --solver=cbc --solver-suffix=dual --solver-options="threads=32" pyvin.py $DIR/data.dat --json --report-timing --stream-solver
+
+# the --stream-solver flag gives verbose output if you want to see what's going on. If you don't care about this, remove it.
+
+# At this point the results.json file should exist. Now we can postprocess. (This does not run in parallel; you could do it separately after the job finishes)
 
 mv results.json $DIR && cd $DIR
 
